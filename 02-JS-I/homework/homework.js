95e3eb89faf9cffffd836736451fcf2a56f939bd
Bhalta_ <mrbhalta@gmail.com>
2022-01-19 19:39:30 -0300

    Homework JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,16 +28,17 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  console.log(str)
+  var miNombre = "Fran";
+  return miNombre;
 }
-devolverString("Esto es una string");
+devolverString(miNombre);
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
   
-}
+}   
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
